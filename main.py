ba5cd4206a1c024acd46fe0a5eb2c6f64b7d291f
Mohamed Salman <mohamed.akj@gmail.com>
2025-05-27 22:56:07 +0530

    Added category creation logic with UI

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ import os
 # Streamlit setup
 st.set_page_config(page_title="Simple Finance App", page_icon="💰", layout="wide")
 
+category_file = "categories.json"
+
+# Different categories of transactions
+# Initialize session state for categories if not already present
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": [],
+    }
+
+# Load categories from JSON file if it exists
+if os.path.exists(category_file):
+    with open(category_file, "r") as f:
+        st.session_state.categories = json.load(f)
+
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+
+
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
@@ -38,6 +57,19 @@ def main():
             # Create a summary of debits and credits
             tab1, tab2 = st.tabs(["Expenses (Debit)", "Incomes (Credits)"])
             with tab1:
+                # Allow user to add the category
+                new_category = st.text_input("New Category Name")
+                add_button = st.button("Add Category")
+
+                if add_button and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.rerun()
+                    else:
+                        st.warning(f"Category '{new_category}' already exists.")
+
+
                 st.write(debits_df)
 
             with tab2:
